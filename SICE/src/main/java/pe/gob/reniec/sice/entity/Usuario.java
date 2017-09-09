e4dc61d6b8d7d8d04e1bb198f2f0c0d58f0223ab
laptopw8 <laptopw8@laptop-windows8>
2017-09-09 16:17:49 -0500

    Configuración del proyecto

---
+++ SICE/src/main/java/pe/gob/reniec/sice/entity/Usuario.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.gob.reniec.sice.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.Proxy;
+
+/**
+ *
+ * @author laptopw8
+ */
+@Entity
+@Table(name = "USUARIO")
+@Proxy(lazy = false)
+public class Usuario implements Serializable {
+
+    @Id
+    @Column(name = "DNI")
+    private String dni;
+    @Column(name = "NOMBRES")
+    private String nombres;
+    @Column(name = "AP_PATERNO")
+    private String apellidoPaterno;
+    @Column(name = "AP_MATERNO")
+    private String apellidoMaterno;
+    @Column(name = "CELULAR_1")
+    private String celular1;
+    @Column(name = "CELULAR_2")
+    private String celular2;
+    @Column(name = "EMAIL_1")
+    private String email1;
+    @Column(name = "EMAIL_2")
+    private String email2;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCelular1() {
+        return celular1;
+    }
+
+    public void setCelular1(String celular1) {
+        this.celular1 = celular1;
+    }
+
+    public String getCelular2() {
+        return celular2;
+    }
+
+    public void setCelular2(String celular2) {
+        this.celular2 = celular2;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+}
